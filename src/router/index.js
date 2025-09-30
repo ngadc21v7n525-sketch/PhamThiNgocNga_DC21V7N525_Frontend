ee50f07d29d9e61865fff7531e5702d8ed85033b
NgocNga <ngadc21v7n525@vlvh.ctu.edu.vn>
2025-09-30 15:03:44 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+import AddContact from "@/views/AddContact.vue";
 const routes = [
 {
 path: "/",
@@ -17,6 +18,11 @@ name: "contact.edit",
 component: () => import("@/views/ContactEdit.vue"),
 props: true // Truyền các biến trong $route.params vào làm props
 },
+ {
+    path: "/add",
+    name: "addcontact",
+    component: AddContact,
+  },
 ];
 const router = createRouter({
 history: createWebHistory(import.meta.env.BASE_URL),
---
+++ src/views/AddContact.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page">
+    <h4>Thêm Liên hệ</h4>
+    <ContactForm :contact="{}" @submit:contact="addContact" />
+  </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+  name: "AddContact",
+  components: {
+    ContactForm,
+  },
+  methods: {
+    async addContact(data) {
+      try {
+        await ContactService.create(data);
+        alert("Liên hệ mới đã được thêm thành công!");
+        this.$router.push({ name: "contactbook" });
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
